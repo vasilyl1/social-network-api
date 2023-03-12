733dfeaad1f8536c4b41e821cc2e61a03dfdf359
vasilyl1 <likhovaido@gmail.com>
2023-03-12 15:48:07 -0400

    user controller functions

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all users
+    getUsers(req, res) {
+        User.find()
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user found with that id' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Delete a single user
+    deleteSingleUser(req, res) {
+        User.deleteOne({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user found with that id' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Update single user
+    updateSingleUser(req, res) {
+        User.updateOne({ _id: req.params.userId }, req.body)
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user found with that id' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Create a user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(500).json(err));
+    },
+};
