224a8b24823367b5493ccd9353dc38183ba4f4f7
vasilyl1 <likhovaido@gmail.com>
2023-03-12 19:57:35 -0400

    POST new user route tested and bug fixed with regex e-mail

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
   {
     username: { type: String, unique: true, required: true, trim: true },
-    email: { type: String, required: true, unique: true, match: '/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/'},
+    email: { type: String, required: true, unique: true, $regex: '/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/'},
     thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought' }],
     friends: [{ type: Schema.Types.ObjectId, ref: 'user' }]
   },
