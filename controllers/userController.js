36393e77ee11caf38701e2b557fb4ca2900df088
vasilyl1 <likhovaido@gmail.com>
2023-03-13 16:44:37 -0400

    get user populated thoughts and friends data bug fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,10 @@ module.exports = {
     },
     // Get a single user
     getSingleUser(req, res) {
-        User.findOne({ _id: req.params.userId })
+        User.findOne(
+            { _id: req.params.userId }
+            ).populate('thoughts')
+            .populate('friends')
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: 'No user found with that id' })
